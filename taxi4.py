d79e1b00bd28fab38f6e60ed38f9c1552dddce2d
Irae Hueck Costa <mail@irae.me>
2025-04-05 14:12:26 -0500

    .

--- taxi4.py
+++ taxi4.py
@@ -7,7 +7,8 @@ import tempfile
 import stat
 
 
-def venv_type(*, pkgs, cmd, python=None):
+def venv_mode(*, venv, pkgs, cmd, python=None):
+    assert venv is None
     yield "uv"
     yield "run"
     yield "--no-project"
@@ -22,19 +23,20 @@ def venv_type(*, pkgs, cmd, python=None):
     yield from shlex.split(cmd)
 
 
-def script_type(*, source):
+def script_mode(*, script):
     with tempfile.NamedTemporaryFile(mode="w", delete=False) as temp_file:
-        temp_file.write(source)
+        temp_file.write(script)
     os.chmod(temp_file.name, stat.S_IXUSR | stat.S_IRUSR | stat.S_IWUSR)
     yield temp_file.name
 
 
-def cmd_type(*, cmd):
+def cmd_mode(*, cmd):
     yield from shlex.split(cmd)
 
 
-def create_docker_type(*, image, image_port, env_prefix=None):
-    def docker_type(*, version=None, port=image_port, **rest):
+def create_docker_mode(*, image, image_port, env_prefix=None):
+    def docker_mode(*, version=None, port=image_port, **rest):
+        version = rest.get(image) or "latest"
         yield "docker"
         yield "run"
         # yield "-ti"
@@ -47,15 +49,13 @@ def create_docker_type(*, image, image_port, env_prefix=None):
                 yield "-e"
                 yield f"{env_prefix}{env.upper()}={env_val}"
 
-        if version:
-            yield f"{image}:{version}"
-        else:
-            yield image
+        yield f"{image}:{version}"
 
-    return docker_type
+    return docker_mode
 
 
-def nix_type(*, pkgs, cmd):
+def nix_mode(*, nix, pkgs, cmd):
+    assert nix is None
     yield "nix-shell"
     yield "--packages"
     yield from [i for i in pkgs.splitlines() if i]
@@ -63,16 +63,27 @@ def nix_type(*, pkgs, cmd):
     yield cmd
 
 
-def raw_type(**kw):
+def raw_mode(**kw):
     yield kw
 
 
-def process_compose_type(*, processes):
+def list_mode(*, list=None):
+    if list is None:
+        list = [section for section in config if section not in ("list", "DEFAULT")]
+    else:
+        list = [i for i in list.splitlines() if i]
+    for task in list:
+        cmd = shlex.join(get_command(task))
+        print(f"{task:16}{cmd}")
+    yield "true"
+
+
+def services_mode(*, services):
     import json
 
-    processes = [i for i in processes.splitlines() if i]
+    services = [i for i in services.splitlines() if i]
     config = {"version": "0.5", "processes": {}}
-    for service in processes:
+    for service in services:
         cmd = get_command(service)
         config["processes"][service] = {"command": shlex.join(cmd)}
     source = json.dumps(config)
@@ -83,24 +94,25 @@ def process_compose_type(*, processes):
     yield temp_file.name
 
 
-types = {
-    "process-compose": process_compose_type,
-    "raw": raw_type,
-    "nix": nix_type,
-    "cmd": cmd_type,
-    "script": script_type,
-    "venv": venv_type,
-    "postgres": create_docker_type(
+modes = {
+    "services": services_mode,
+    "raw": raw_mode,
+    "list": list_mode,
+    "nix": nix_mode,
+    "cmd": cmd_mode,
+    "script": script_mode,
+    "venv": venv_mode,
+    "postgres": create_docker_mode(
         image_port=5432,
         image="postgres",
         env_prefix="POSTGRES_",
     ),
-    "mysql": create_docker_type(
+    "mysql": create_docker_mode(
         image_port=3306,
         image="mysql",
         env_prefix="MYSQL_",
     ),
-    "redis": create_docker_type(
+    "redis": create_docker_mode(
         image_port=6379,
         image="redis",
     ),
@@ -109,21 +121,40 @@ types = {
 
 config = configparser.ConfigParser(
     interpolation=configparser.ExtendedInterpolation(),
-    # allow_no_value=True,
+    allow_no_value=True,
 )
+
+
+def infer_mode(section_name, section):
+    mode = section.get("mode", None)
+    if not section:
+        return section_name
+    if not mode:
+        return next(iter(section.keys()))
+    return mode
 
 
 def get_command(name):
     section = config[name]
     section = dict(section)
-    type_ = section.pop("type")
-    handler = types[type_]
+    mode = infer_mode(name, section)
+    section.pop("mode", None)
+    handler = modes[mode]
     cmd = handler(**section)
     return list(cmd)
 
 
-config.read("taxi.ini")
-run = sys.argv[1]
-cmd = get_command(run)
-print(cmd)
-subprocess.call(cmd)
+def main():
+    config.read("taxi.ini")
+    try:
+        run = sys.argv[1]
+    except IndexError:
+        print("missing arg")
+        sys.exit(1)
+    cmd = get_command(run)
+    print(cmd)
+    subprocess.call(cmd)
+
+
+if __name__ == "__main__":
+    main()
